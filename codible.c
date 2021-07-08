fe85f759b3b4b3b8924ae4d1d06e4904faa5b388
Hridoy-31 <raihanulbashirhridoy@gmail.com>
2021-07-08 22:17:56 +0600

    Timeout implementation

--- codible.c
+++ codible.c
@@ -1,7 +1,7 @@
 #include <ctype.h> // iscntrl() resides in it
 #include <stdio.h> // printf() resides in it
 #include <stdlib.h> // atexit() resides in it 
-#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON, ISIG, IXON reside in it
+#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON, ISIG, IXON. IEXTEN, ICRNL, OPOST, BRKINT, INPCK, ISTRIP, CS8, VMIN, VTIME reside in it
 #include <unistd.h> // read(), STDIN_FILENO reside in it
 
 struct termios original;
@@ -15,8 +15,12 @@ void enableRawMode() {
   atexit(disableRawMode); 
 
   struct termios raw = original;
-  raw.c_iflag &= ~(IXON); // IXON used to ignore XOFF and XON
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG); // ICANON used for reading input byte by byte & ISIG used to ignore SIGINT and SIGTSTP
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // IXON used to ignore XOFF and XON & ICRNL used to handle Carriage Return (CR) and New Line (NL), BRKINT used to handle SIGINT, INPCK used to handle Parity Check, ISTRIP used to handle 8-bit stripping, CS8 used to handle character size (CS) to 8 bits per byte
+  raw.c_oflag &= ~(OPOST); // OPOST used to handle post-processing output
+  raw.c_cflag |= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // ICANON used for reading input byte by byte & ISIG used to ignore SIGINT and SIGTSTP, IEXTEN used to disable Ctrl-o and Ctrl-V
+  raw.c_cc[VMIN] = 0; // control characters for terminal settings
+  raw.c_cc[VTIME] = 1; // control characters for terminal settings
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // Setting up modified terminal attributes
 }
 
@@ -24,14 +28,19 @@ int main()
 {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') // checking 'q' to quit
-  {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) // check it's printable or not
     {
-      printf("%d\n", c); // Just the ASCII value
+      printf("%d\r\n", c); // Just the ASCII value
     }
     else {
-      printf("%d ('%c')\n", c, c); // ASCII value & the corresponding character
+      printf("%d ('%c')\r\n", c, c); // ASCII value & the corresponding character
+    }
+    if (c == 'q') // checking 'q' for quit
+    {
+      break;
     }
   }
   return 0;
